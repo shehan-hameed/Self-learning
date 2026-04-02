9cfc78420b45d3921eb5951bd1090e0f8ff3c25f
shehan-hameed <shehan2005savain@gmail.com>
2026-04-02 23:30:21 +0530

    lesson 2 exercise

---
+++ C/student.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#define year 2026
+
+int main(){
+    //variables
+ int age = 20;
+ double gpa = 3.80;
+ char grade = 'A';
+ 
+ //output
+ printf("--- Student Profile ---\n");
+ printf("Age:   %d\n",age);
+ printf("GPA:   %.2lf\n", gpa);
+ printf("Grade: %c\n", grade);
+ printf("Year:  %d\n", year);
+
+ return 0;
+
+}
