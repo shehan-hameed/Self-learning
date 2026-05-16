3363c8905c0f2b56f5a5ff490b56d09aae8923d9
shehan-hameed <shehan2005savain@gmail.com>
2026-05-16 15:04:40 +0530

    Programize lessons

---
+++ C/Programize full course/ep2-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int firstNumber=33;
+    printf("Frist number is %d", firstNumber);
+    
+    int secondNumber = firstNumber;
+    printf("\nSecond number is %d", secondNumber);
+    
+    
+    return 0;
+}
---
+++ C/Programize full course/ep2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int age =21;
+    
+    printf("My age is %d", age);
+    
+    age =22;
+    printf("\nI will turn %d next year",age);
+    
+    return 0;
+}
