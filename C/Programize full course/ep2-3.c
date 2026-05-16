ab4a7f3a9c3ee85fcd6a1050e72ddf3d156a711e
shehan-hameed <shehan2005savain@gmail.com>
2026-05-16 15:22:40 +0530

    Multiple variables in single line

---
+++ C/Programize full course/ep2-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int variable1 = 10, variable2 = 20;
+    printf("This is the frist varibale: %d.\nThis the second varibale: %d .", variable1,variable2);
+    
+    
+    
+    
+    
+    return 0;
+}
